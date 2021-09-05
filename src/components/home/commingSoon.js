fa081087cd6ea5978d7a8c88eeeb22e60091b2dd
yutakaki <takaki.yu.dev@gmail.com>
2021-09-05 21:18:17 +0900

    FIXED SLIDESHOW

--- src/components/home/commingSoon.js
+++ src/components/home/commingSoon.js
@@ -2,27 +2,49 @@ import React, { useEffect, useState} from 'react';
 import {usePrevious} from 'react-use';
 import { randomNumber } from '../../methods/method';
 import sampleCover from '../../assets/sample.jpg';
+import axios from 'axios';
 
 const CommingSoon = () => {
-    const [showNum, setShowNum] = useState(0);
-    const [timeOut, setTimeOut] = useState()
+    const [showNum, setShowNum] = useState();
+    const [timeOut, setTimeOut] = useState();
     const prevShowNum = usePrevious(showNum);
     const [throttleTime, setThrottleTime] = useState(0);
-    const [commingSoon, setCommingSoon] = useState([1, 2, 3, 4, 5])
+    const [commingSoon, setCommingSoon] = useState([])
 
     const colors = ['#62bccc', '#337ac0', '#ee9323', '#F2b922']
 
     const setShowNumTimeout = () => {
-        setTimeOut(setTimeout(() => {
-            
+        setTimeOut(setTimeout(() => {            
             setShowNum(showNum >= commingSoon.length - 1 ? 0 : showNum + 1);
-        }, 2000))
+        }, 5000))
         
     }
     useEffect(() => {
-        setShowNumTimeout()
+        
+        (async() => {
+            try {
+                const commingSoonRequest = await axios.get('https://api.themoviedb.org/3/trending/all/day?api_key=2effcb37ac7b1550616d653eea9cb4d6');
+
+                const data = commingSoonRequest.data.results.filter(data => data.media_type !== 'person');
+                console.log(data);
+
+                setCommingSoon(data);
+                setTimeOut(setTimeout(() => {            
+                    setShowNum(0);
+                }))
+            
+                
+            } catch (error) {
+                console.log(error);
+                
+            }
+
+        } )();
+        
+        
     }, []);
-    const showInfo = document.querySelectorAll('.showInfo')
+    const showInfo = document.querySelectorAll('#commingSoon .showInfo');
+    const showPoster = document.querySelectorAll('#commingSoon .poster')
 
     useEffect(() => {
         showInfo.forEach((element, i) => {
@@ -56,6 +78,35 @@ const CommingSoon = () => {
             
         });
 
+        showPoster.forEach((element, i) => {
+            if(showNum === 0){
+                if(i === commingSoon.length - 1){
+                    element.style.transition = '0s';
+                }   
+            //when user clicks previous button and reaches the end of the show, the transition of both first and last show would be none
+            }else if(prevShowNum === 0 && showNum === commingSoon.length - 1){
+                element.style.transition = '0s';
+
+            }else{
+                element.style.transition = '.5s';
+            }
+
+
+            //if the current show is being viewed, the opacitiy would be 1
+            if(showNum === i){
+                element.classList.remove('hidePoster');
+            //this is to hide the elements for moving 
+            }else{
+                element.classList.add('hidePoster');
+            }
+            if(i <= showNum - 2){
+                element.style.transform = `translateY(${100 * (showNum - 1) }%)`;
+            }else{    
+                element.style.transform = `translateY(${-100 * (showNum) }%)`;
+            }
+
+        })
+
         setShowNumTimeout();
         
         
@@ -63,9 +114,10 @@ const CommingSoon = () => {
     }, [showNum]);
 
     const changeShow = (val) => {
+        clearTimeout();
 
         const time = new Date().getTime();
-        if(time > throttleTime){
+        if(time > throttleTime ){
             console.log(true);
             setThrottleTime(time + 500);
             clearTimeout(timeOut);
@@ -87,7 +139,7 @@ const CommingSoon = () => {
         <div id='commingSoon'>
             <div className='containerOpacity'></div>
             <section className='commingSoonContainer'> 
-                <button className='slideBtn prev' onClick={() => changeShow(-1)}>prev</button>
+                <i className='slideBtn fa fa-arrow-circle-left' onClick={() => changeShow(-1)}></i>
                 <div className='showinfoContainer'>
                     {commingSoon.map((item, i) => (
                         <div className='showInfo'>
@@ -120,11 +172,14 @@ const CommingSoon = () => {
                 </div>
                 <div className='showPosterContainer'>
                     {commingSoon.map(show => (
-                        <img src={sampleCover} />
+                        <div className='poster'>
+                            <img src={sampleCover} />
+                        </div>
+                        
                     ))}
 
                 </div>
-                <button className='slideBtn next' onClick={() => changeShow(1)}>next</button>
+                <i className='slideBtn fa fa-arrow-circle-right' onClick={() => changeShow(1)}></i>
             </section>
 
         </div>
